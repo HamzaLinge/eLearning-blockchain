f5a65f5bb80ff5fb013c2d346aa9a183cb823f94
Baghdad <BaghdadHamdoud@gmail.com>
2022-06-15 00:06:13 +0100

    text centré

--- client/src/features/homePage/HomePage.js
+++ client/src/features/homePage/HomePage.js
@@ -18,24 +18,24 @@ function HomePage() {
 
     useEffect(() => {
         const idSetInterval = setInterval(() => {
-            // const allSpan = document.querySelectorAll(".homePage__images__indicator span");
+            const allSpan = document.querySelectorAll(".homePage__images__indicator span");
             if(rightDirection){
-                // allSpan[position].style.backgroundColor = "transparent";
+                allSpan[position].style.backgroundColor = "transparent";
                 position++;
                 if(position === imgPathArray.length - 1){
                     rightDirection = !rightDirection;
                 }
                 document.querySelector(".homePage__images__wrapper").style.transform = 'translateX(-'+String(Math.abs(position * IMG_WIDTH))+'px)';
-                // allSpan[position].style.backgroundColor = "#fbfbfb";
+                allSpan[position].style.backgroundColor = "#fbfbfb";
             }
             else{
-                // allSpan[position].style.backgroundColor = "transparent";
+                allSpan[position].style.backgroundColor = "transparent";
                 position--;
                 if(position === 0){
                     rightDirection = !rightDirection;
                 }
                 document.querySelector(".homePage__images__wrapper").style.transform = 'translateX(-'+String(Math.abs(position * IMG_WIDTH))+'px)';
-                // allSpan[position].style.backgroundColor = "#fbfbfb";
+                allSpan[position].style.backgroundColor = "#fbfbfb";
             }
         }, 3000);
         dispatch(setIdMyInterval(idSetInterval));
@@ -53,27 +53,36 @@ function HomePage() {
                         ))
                     }
                 </div>
-                {/*<div className="homePage__images__indicator">*/}
-                {/*    {*/}
-                {/*        imgPathArray.map(imgPath => (<span></span>))*/}
-                {/*    }*/}
-                {/*</div>*/}
+                <div className="homePage__images__indicator">
+                    {
+                        imgPathArray.map(imgPath => (<span></span>))
+                    }
+                </div>
             </div>
             {/*<div className="homePage__title">*/}
             {/*    <p className="homePage__title__item homePage__title__blockchained">Blockchained</p>*/}
             {/*    <p className="homePage__title__item homePage__title__learning">Learning</p>*/}
             {/*    <p className="homePage__title__item homePage__title__certification">Certification</p>*/}
             {/*</div>*/}
-            <div className="homePage__main">
-                <div className="homePage__main__text">
-                    <strong>BCLC</strong> is a learning platform,
-                    where at the end of the MCQ of a course,
-                    you will obtain a certificate,
-                    attesting to your acquired skills.
-                </div>
+            {/*<div className="homePage__main">*/}
+            {/*    <div className="homePage__main__text">*/}
+            {/*        <strong>BCLC</strong> is a learning platform,*/}
+            {/*        where at the end of the MCQ of a course,*/}
+            {/*        you will obtain a certificate,*/}
+            {/*        attesting to your acquired skills.*/}
+            {/*    </div>*/}
                 {/*<div className={"homePage__main__text"}>*/}
                 {/*    // Zone de texte*/}
                 {/*</div>*/}
+            {/*</div>*/}
+            <div className="homePage__text">
+                <strong>BCLC</strong> is a learning platform,
+                where at the end of the MCQ of a course,
+                you will obtain a certificate,
+                attesting to your acquired skills.
+            </div>
+            <div className="homePage__logo">
+                <img src={"Logo-black.png"} alt={""} className={"homePage__logo__img"} />
             </div>
         </div>
     );
